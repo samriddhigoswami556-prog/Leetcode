d0bbf9cd88cbce79121c9c171e143132fa65e76e
Samriddhi Goswami <samriddhigoswami556@gmail.com>
2026-07-15 16:23:07 +0530

    Time: 0 ms (100%), Space: 8.3 MB (63.77%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    vector<string> split(string a) {
-        vector<string> v;
-        int i = 0;
-        while (i < a.size()) {
-            string word = "";
-            while (i < a.size() && a[i] != '.') {
-                word.push_back(a[i]);
-                i++;
-            }
-            v.push_back(word);
-            i++;
-        }
-        return v;
-    }
-    int compareVersion(string version1, string version2) {
-        vector<string> arr1 = split(version1);
-        vector<string> arr2 = split(version2);
-        int i = 0;
-        while (i < arr1.size() || i < arr2.size()) {
+    int compareVersion(string ver1, string ver2) {
+        int i = 0, j = 0;
+        int n = ver1.size();
+        int m = ver2.size();
+        while (i < n || j < m) {
             int rev1 = 0;
             int rev2 = 0;
-            if (i < arr1.size())
-                rev1 = stoi(arr1[i]);
-            if (i < arr2.size())
-                rev2 = stoi(arr2[i]);
+            while (i < n && ver1[i] != '.') {
+                rev1 = rev1 * 10 + (ver1[i] - '0');
+                i++;
+            }
+            while (j < m && ver2[j] != '.') {
+                rev2 = rev2 * 10 + (ver2[j] - '0');
+                j++;
+            }
             if (rev1 > rev2)
                 return 1;
             else if (rev1 < rev2)
                 return -1;
             i++;
+            j++;
         }
         return 0;
     }
